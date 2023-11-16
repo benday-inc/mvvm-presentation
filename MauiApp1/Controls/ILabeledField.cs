728a360bfa927a8c5d09653a8cf508db2ed4e522
Benjamin Day <benday@benday.com>
2023-11-16 10:30:24 -0500

    creating textbox field

---
+++ MauiApp1/Controls/ILabeledField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Controls;
+public interface ILabeledField
+{
+    void SetLabelText(string value);
+}
