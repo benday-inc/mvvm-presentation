a57230545468f80d1ce35bd948af91582b7ca6db
Benjamin Day <benday@benday.com>
2023-11-17 13:08:32 -0500

    Trying to create simple sample

---
+++ Benday.Presentation/ValueConverters/BendayValueConverterBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Benday.Presentation.ValueConverters;
+
+public abstract class BendayValueConverterBase : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return ConvertTo(value, targetType);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return ConvertFrom(value, targetType);
+    }
+
+    protected abstract object? ConvertTo(object? value, Type targetType);
+    protected abstract object? ConvertFrom(object? value, Type targetType);
+}
