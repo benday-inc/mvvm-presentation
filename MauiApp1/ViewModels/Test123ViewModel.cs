b7d2c0e36f9b8158503911ac41ff46f55a7c4207
Benjamin Day <benday@benday.com>
2023-12-29 17:56:18 +0400

    Implemented combobox field but initial selection isn't working

--- MauiApp1/ViewModels/Test123ViewModel.cs
+++ MauiApp1/ViewModels/Test123ViewModel.cs
@@ -11,4 +11,16 @@ public class Test123ViewModel : ViewModelBase
     public ViewModelField<string> StringField { get; private set; } = new ViewModelField<string>(String.Empty);
     public ViewModelField<int> IntField { get; private set; } = new ViewModelField<int>(123);
 
+    public SingleSelectListViewModel SingleSelectField { get; private set; } = new SingleSelectListViewModel(GetSampleItems());
+    private static IEnumerable<ISelectableItem> GetSampleItems()
+    {
+        var items = new List<ISelectableItem>();
+
+        items.Add(new SelectableItem(false, "Item 1"));
+        items.Add(new SelectableItem(true, "Item 2"));
+        items.Add(new SelectableItem(false, "Item 3"));
+
+        return items;
+    }
+
 }
