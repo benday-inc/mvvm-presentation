7de42398572f58517951ffe2e495c2d0815dc972
Benjamin Day <benday@benday.com>
2024-01-11 10:20:20 -0500

    Created new project

---
+++ Benday.Presentation.Controls/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,6 @@
+﻿namespace Benday.Presentation.Controls;
+
+// All the code in this file is only included on Android.
+public class PlatformClass1
+{
+}
---
+++ Benday.Presentation.Controls/Platforms/Tizen/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Benday.Presentation.Controls;
+// All the code in this file is only included on Tizen.
+public class PlatformClass1
+{
+}
