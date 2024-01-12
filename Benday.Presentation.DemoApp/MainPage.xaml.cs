9f0bfe88cd419dba29ac2417f839f7ee7724924d
Benjamin Day <benday@benday.com>
2024-01-12 10:35:00 -0500

    Fixed viewmodel binding

--- Benday.Presentation.DemoApp/MainPage.xaml.cs
+++ Benday.Presentation.DemoApp/MainPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace Benday.Presentation.DemoApp;
+﻿using Benday.Presentation.DemoApp.ViewModels;
+
+namespace Benday.Presentation.DemoApp;
 
 public partial class MainPage : ContentPage
 {
+    private MessageBoxMessageManager _MessageManager = new();
+
     public MainPage()
     {
         InitializeComponent();
-    }    
+
+        this.BindingContext = new MainPageViewModel(_MessageManager);
+    }   
 }
 
